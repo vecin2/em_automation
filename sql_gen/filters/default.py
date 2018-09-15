d27b00362ad47d2b683e2a34780ee67b5486d90f
vecin2 <vecin2@gmail.com>
2018-09-15 16:22:53 -0400

    implement exception when default filter invoke with no Constant

--- sql_gen/filters/default.py
+++ sql_gen/filters/default.py
@@ -3,7 +3,11 @@ class DefaultFilter(object):
         self.filter = jinja_filter;
 
     def apply(self, prompt_text):
-        default_unicode= self.filter.args[0].value
-        prompt_text = prompt_text + " (default is "+default_unicode+")"
-        return prompt_text
+        if (len (self.filter.args) is 1 and
+            hasattr(self.filter.args[0],"value")):
+                default_unicode= self.filter.args[0].value
+                prompt_text = prompt_text + " (default is "+default_unicode+")"
+                return prompt_text
 
+        raise ValueError("Default Filters at the moment only support "+\
+                    "constant values, a variable was passed "+str(self.filter.args[0]))
--- sql_gen/templates/rewire_verb.sql
+++ sql_gen/templates/rewire_verb.sql
@@ -1,8 +1,20 @@
+{#
+SELECT V.* FROM EVA_PROCESS_DESCRIPTOR PD, 
+EVA_VERB V,
+EVA_PROCESS_DESC_REFERENCE PDR
+WHERE 
+V.PROCESS_DESC_REF_ID = PDR.ID
+AND PDR.PROCESS_DESCRIPTOR_ID = PD.ID
+AND V.ENTITY_DEF_ID = 850
+AND V.NAME = 'inlineSearch'
+#}
+{#{{ config_id | ad_run_query (query)}} #}
+{#{{ set old_pd = find_pd_by_verb_name_and_path(verb_name,repository_path)}} #}
+{#{{ type_id | default(old_pd=pd['TYPE_ID'}}) #}
+
+
 {# We set compute descriptor id #}
 {% set process_descriptor_id = "GSC"+ entity_def_id  + verb_name%}
-{% set query = "SELECT CONFIG_ID FROM EVA_PROCESS_DESCRIPTOR WHERE ENTITY_DEF_ID =" + entity_def_id %}
-this is the query  {{query}}
-{{ config_id | default (query)}}
 {% include 'add_process_descriptor.sql' %}
 {% set process_descriptor_ref_id = process_descriptor_id %}
 {% include 'add_process_descriptor_ref.sql' %}
