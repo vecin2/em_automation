b342e14f440b4ac025e42b2c8c5f761e500c483b
vecin2 <vecin2@gmail.com>
2018-11-28 16:49:58 -0500

    adding test for logger

--- sql_gen/test/test_logger.py
+++ sql_gen/test/test_logger.py
@@ -1,7 +1,10 @@
 import pytest
-import sql_gen.logger.log as log
+from sql_gen.app_project import AppProject
+from sql_gen.emproject import EMProject
+#from sql_gen.loggu import log
+from sql_gen.logger import log
 
-
+@pytest.mark.skip
 def test_log_path_comes_from_config(fs):
     log_file_path="log.log"
     config ={"LOG_FILE":log_file_path}
@@ -11,4 +14,14 @@ def test_log_path_comes_from_config(fs):
     with open(log_file_path, 'r') as content_file:
         content = content_file.read()
     assert message == content
+
+def test_default_log_config():
+    root =  "/my_project/sqltask"
+    import pdb; pdb.set_trace()
+    config = log.config(root)
+
+    info_log_path =config['handlers']['info_file_handler']['filename']
+    error_log_path =config['handlers']['error_file_handler']['filename']
+    assert "/my_project/sqltask/logs/info.log" == info_log_path
+    assert "/my_project/sqltask/logs/error.log" == error_log_path
 
