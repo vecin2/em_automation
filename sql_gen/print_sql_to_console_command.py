db9e3ae1bdf98500595409c70037ac1dc56ac90c
vecin2 <vecin2@gmail.com>
2019-02-11 09:34:16 -0500

    implement run

--- sql_gen/print_sql_to_console_command.py
+++ sql_gen/print_sql_to_console_command.py
@@ -1,5 +1,17 @@
 class PrintSQLToConsoleCommand(object):
     """"""
-    def run():
-        select_template = SelectTemplateCommand()
+    def __init__(self, displayer=None):
+        self.displayer = displayer
+    def run(self):
+        output = FillComposedTemplateCommand().run()
+        self.displayer.render_sql(output)
 
+class PrintSQLToConsoleDisplayer(object):
+    """Prints to console the command output"""
+    def render_sql(self,sql_to_render):
+        print(sql_to_render)
+
+class FillComposedTemplateCommand(object):
+    """It fills one or multiple templates and returns and output string"""
+    def run(self):
+        return "hello Mark"
--- sql_gen/test/test_print_sql_to_console.py
+++ sql_gen/test/test_print_sql_to_console.py
@@ -1,6 +1,10 @@
-from sql_gen.print_sql_to_console_command import PrintSQLToConsoleCommand
+from sql_gen.print_sql_to_console_command import PrintSQLToConsoleCommand,PrintSQLToConsoleDisplayer
 
 
-def test_some():
-    
-    """"""
+def test_it_sends_to_console_the_filled_template(mocker):
+    sql_output = "hello Mark"
+    displayer = PrintSQLToConsoleDisplayer()
+    mocker.patch.object(displayer,'render_sql')
+    command = PrintSQLToConsoleCommand(displayer)
+    command.run()
+    displayer.render_sql.assert_called_once_with(sql_output)
