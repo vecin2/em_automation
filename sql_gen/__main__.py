b1b29267156a33dcd3714bbfa59a084e341dcc51
vecin2 <vecin2@gmail.com>
2019-03-04 19:09:29 -0500

    cleaning source files

--- sql_gen/__main__.py
+++ sql_gen/__main__.py
@@ -1,13 +1,7 @@
-from sql_gen import sqltaskapp
-from sql_gen.command_factory import CommandFactory 
 from sql_gen.command_line_app import CommandLineSQLTaskApp
 ##main
 def main():
-    #sqltaskapp.init()
-    #sqltaskapp.run()
-    app_test_factory = CommandFactory()
-    app = CommandLineSQLTaskApp(app_test_factory)
-    app.run()
+    CommandLineSQLTaskApp().run()
 
 
 if __name__ == '__main__':
