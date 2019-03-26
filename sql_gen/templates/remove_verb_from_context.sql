6b3b8657e1fd09edc89c40237fac2c46aa1389c6
vecin2 <vecin2@gmail.com>
2019-03-26 16:41:42 -0400

    remove templates

--- sql_gen/templates/remove_verb_from_context.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM EVA_CONTEXT_VERB_ENTRY
-where CONFIG_ID = @CC.{{config_id | description("config_id (e.g. Home, CustomerPostIdentify, Customer)")}}
-and VERB = '{{verb_name}}'
-and ENTITY_DEF_TYPE_ID= @ET.{{customer_type}};
