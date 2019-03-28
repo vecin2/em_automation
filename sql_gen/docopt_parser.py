2689b953b9ffd6af1cbc14901aed4faa1dec3647
vecin2 <vecin2@gmail.com>
2019-03-28 08:27:07 -0400

    Add examples of use

--- sql_gen/docopt_parser.py
+++ sql_gen/docopt_parser.py
@@ -1,7 +1,15 @@
 """dtask.
 Usage:
-  dtask print-sql
-  dtask create-sql [<directory>]
+    dtask print-sql
+    dtask create-sql [<directory>]
+    dtask -h | --help
+
+Examples:
+    dtask create-sql
+    dtask create-sql modules/PCCoreContactHistory/sqlScripts/oracle/updates/Pacificorp_R_0_0_1/overrideViewContactHistory
+
+Options:
+    -h --help     Show this screen.
 """
 from docopt import docopt
 
