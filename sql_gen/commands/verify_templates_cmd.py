92b9b5c29983585de2a002612e1ae98588f22087
vecin2 <vecin2@gmail.com>
2019-04-18 13:19:25 -0400

    extract TestSQLFile

--- sql_gen/commands/verify_templates_cmd.py
+++ sql_gen/commands/verify_templates_cmd.py
@@ -71,13 +71,68 @@ class TestTemplatesCommandDisplayer(object):
     def test_folder_does_no_exist(self,directory):
         print("Test folder '"+directory+"' does not exist.")
 
+class TestLoader(object):
+    def __init__(self,testpath,templates_path):
+        self.testpath=testpath
+        self.templates_path=templates_path
+        self.parser = TestFileParser()
+    def load_tests(self):
+        return self._get_valid_tests()
+
+    def _get_valid_tests(self):
+        result =[]
+        testpath = self.testpath
+        for filename in os.listdir(testpath):
+            filepath = os.path.join(testpath,filename)
+            if self._is_valid_test_file(filepath):
+                result.append(TestSQLFile(filepath))
+        return result
+
+    def _is_valid_test_file(self,filepath):
+            filename = os.path.basename(filepath)
+            extension = os.path.splitext(filename)[1]
+            return os.path.isfile(filepath)\
+                    and extension==".sql"\
+                    and self._matches_template(filename)
+
+    def _matches_template(self, test_file):
+        for template_file in os.listdir(self.templates_path):
+            if template_file == self.parser._extract_template_filename(test_file):
+                return True
+
+        return False
+
+
+class TestSQLFile(object):
+    def __init__(self,filepath):
+        self.filepath = filepath
+        self.parser = TestFileParser()
+        test_file = open(self.filepath,"r+")
+        self.content =test_file.read()
+        test_file.close()
+
+    def filename(self):
+        return os.path.basename(self.filepath) 
+
+    def template_filename(self):
+        return self.parser._extract_template_filename(self.filename())
+
+    def template_name(self):
+        return os.path.splitext(self.template_filename())[0]
+
+    def expected_sql(self):
+        return self.parser.parse_expected_sql(self.content)
+
+    def values(self):
+        return self.parser.parse_values(self.content)
+
+
 class TestTemplatesCommand(object):
     def __init__(self,
                  pytest=pytest,
                  templates_path=None,
                  emprj_path=None,
                  initial_context=None):
-        self.templates_path = templates_path
         self.emprj_path = emprj_path
         self.initial_context = initial_context
         self.apprunner = FileAppRunner(templates_path, 
@@ -87,7 +142,7 @@ class TestTemplatesCommand(object):
         self.pytest =pytest
         self.displayer = TestTemplatesCommandDisplayer()
         self.source_builder = SourceTestBuilder()
-        self.parser = TestFileParser()
+        self.test_loader = TestLoader(self.all_tests_path,templates_path)
 
     def generated_test_filepath(self):
             return self._tmp_folder()+"/test_all_templates.py"
@@ -109,52 +164,25 @@ class TestTemplatesCommand(object):
         self._create_test_file(self._generate_all_tests())
         self.pytest.main(['-x','-v',self._tmp_folder()])
 
-    def _generate_all_tests(self):
-        testpath = self.all_tests_path
-        for filename in os.listdir(testpath):
-            filepath = os.path.join(testpath,filename)
-            if self._is_valid_test_file(filepath):
-                self._generate_test(filepath)
-        return self.source_builder.content
-
-    def _is_valid_test_file(self,filepath):
-            filename = os.path.basename(filepath)
-            extension = os.path.splitext(filename)[1]
-            return os.path.isfile(filepath)\
-                    and extension==".sql"\
-                    and self._matches_template(filename)
-
-    def _matches_template(self, test_file):
-        for template_file in os.listdir(self.templates_path):
-            if template_file == self._extract_template_filename(test_file):
-                return True
-
-        return False
-
-    def _generate_test(self,filepath):
-        filename =os.path.basename(filepath) 
-        test_file = open(filepath,"r+")
-        expected = self.parser.parse_expected_sql(test_file.read())
-        actual =self.apprunner._run_test(filepath)
-        template_file = self._extract_template_filename(filename)
-        template_name = os.path.splitext(template_file)[0]
-        self.source_builder.add_expected_sql_test(template_name=template_name,
-                                          expected=expected,
-                                          actual=actual)
-
-    def _extract_template_filename(self,filename):
-        return self._remove_prefix("test_",filename)
-
-    def _remove_prefix(self,prefix, string):
-        if string.startswith(prefix):
-            return string[len(prefix):].strip()
-        return string
-
     def _create_test_file(self,source):
         if source:
             test_file = open(self.generated_test_filepath(),"w")
             test_file.write(source)
             test_file.close()
+
+    def _generate_all_tests(self):
+        for testfile in self.test_loader.load_tests():
+            self._generate_test(testfile)
+        return self.source_builder.content
+
+    def _generate_test(self,testfile):
+        filepath = testfile.filepath
+        expected = testfile.expected_sql()
+        actual =self.apprunner.run_test(testfile)
+        template_name = testfile.template_name()
+        self.source_builder.add_expected_sql_test(template_name=template_name,
+                                          expected=expected,
+                                          actual=actual)
 
 
 class FillTemplateAppRunner():
@@ -176,36 +204,34 @@ class FillTemplateAppRunner():
         self.inputs.append(user_input)
         return self
 
+    def clear_inputs(self):
+        self.inputs.clear()
+
+    def run(self):
+        str_inputs ="\n".join([input for input in self.inputs])
+        sys.stdin = StringIO(str_inputs)
+        self._run()
+
     def teardown(self):
         sys.stdin = self.original_stdin
 
 class FileAppRunner(FillTemplateAppRunner):
     def __init__(self,templates_path,emprj_path, initial_context=None):
         super().__init__()
-        self.parser =TestFileParser()
         self.print_sql_cmd = PrintSQLToConsoleCommand(
                             initial_context=initial_context,
                             emprj_path=emprj_path,
                             templates_path =templates_path)
 
-    def _run_test(self,filepath):
-        sys.stdin = StringIO(self._user_input_to_str(filepath))
+    def run_test(self,testfile):
+        self.clear_inputs()
+        self.select_template(testfile.template_filename(),
+                             testfile.values())
+        self.saveAndExit()
         self.run()
-        test_file = open(filepath,"r+")
-        expected = self.parser.parse_expected_sql(test_file.read())
         return self.print_sql_cmd.sql_printed()
 
-    def run(self):
+    def _run(self):
         self.print_sql_cmd.run()
-        self.inputs.clear()
         self.teardown()
 
-    def _user_input_to_str(self,filepath):
-        filename =os.path.basename(filepath) 
-        with open(filepath) as f:
-             template_name =self.parser._extract_template_filename(filename)
-             temp_values = self.parser.parse_values(f.read())
-             self.select_template(template_name,temp_values)
-        self.saveAndExit()
-        inputs = self.inputs
-        return "\n".join([input for input in inputs])
