9ab129b5cc707b798812fbf667355b6ce52b6a5f
vecin2 <vecin2@gmail.com>
2019-05-02 16:26:07 -0400

    changing template

--- sql_gen/templates/add_activity_to_perspective.sql
+++ sql_gen/templates/add_activity_to_perspective.sql
@@ -2,19 +2,23 @@
 {% set tmp = entity_type | suggest(_keynames.ET) %}
 {% set verb_names = _db.list.v_names_by_et(entity_type) %}
 {% set tmp = verb_name | suggest(verb_names)%} 
-{% set perspective_verbs = _db.fetch.v_displaynames_by_pers_id(perspective_id) %}
+{% set perspective_verbs = _db.fetch.v_by_pers_id(perspective_id) %}
 {%set seq_no_desc =perspective_verbs.to_str()+"\n"+"sequence_number"%}
 {% set tmp =sequence_number | description(seq_no_desc) %}
 {% set to_update = perspective_verbs.where("SEQUENCE_NUMBER>="+sequence_number)%}
+
+--updating sequence numbers
 {% for item in to_update %}
 
 UPDATE EVA_CONTEXT_VERB_ENTRY
 SET SEQUENCE_NUMBER = {{ item.SEQUENCE_NUMBER + 1 }}
-where CONFIG_ID = {{item.CONFIG_ID}}
-and ENTITY_DEF_TYPE_ID = {{item.ENTITY_DEF_TYPE_ID}}
+where CONFIG_ID = @CC.{{perspective_id}}
+and ENTITY_DEF_TYPE_ID = @ET.{{item.ET_KEYNAME}}
 and VERB = '{{item.VERB}}'
+
 {% endfor %}
 
+--inserting new activity in specific sequence no
 INSERT INTO EVA_CONTEXT_VERB_ENTRY (CONFIG_ID,CONFIG_ENV_ID,VERB,ENTITY_DEF_TYPE_ID,ENTITY_DEF_TYPE_ENV_ID,SEQUENCE_NUMBER,RELEASE_ID) 
 VALUES (
 	 @CC.{{perspective_id}},--config_id
