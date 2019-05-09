c2e7d7c5098bccade7ecbe64a5a048f18b0212d3
vecin2 <vecin2@gmail.com>
2019-05-09 18:43:56 -0400

    remove form idmap table when removing entities

--- sql_gen/templates/remove_verb_from_entity.sql
+++ sql_gen/templates/remove_verb_from_entity.sql
@@ -1,15 +1,14 @@
 {% set tmp = entity_id | suggest(_keynames.ED) %}
 {% set entity_verbs = _db.fetch.verbs_by_ed(entity_id) %}
-{% set verb_names = entity_verbs.column('NAME') %}
-{% set tmp = verb_name | suggest(verb_names) %}
-{% set verb =  entity_verbs.find(NAME=verb_name) %}
+{% set verb_ids = entity_verbs.column('KEYNAME') %}
+
 DELETE
 FROM EVA_VERB
-WHERE NAME='{{verb_name}}'
+WHERE ID = @V.{{verb_id | suggest(verb_ids)}}
 and ENTITY_DEF_ID=@ED.{{entity_id}};
 
 
 DELETE
 FROM CCADMIN_IDMAP
 WHERE KEYSET = 'V'
-AND ID =@V.{{verb["KEYNAME"]}};
+AND ID =@V.{{verb_id}};
--- sql_gen/test_templates/test_remove_verb_from_entity.sql
+++ sql_gen/test_templates/test_remove_verb_from_entity.sql
@@ -1,10 +1,11 @@
---["Customer","inlineSearch"]
+--["Customer","InlineSearchCustomer"]
 DELETE
 FROM EVA_VERB
-WHERE NAME='inlineSearch'
+WHERE ID =@V.InlineSearchCustomer
 and ENTITY_DEF_ID=@ED.Customer;
 
 DELETE
 FROM CCADMIN_IDMAP
 WHERE KEYSET = 'V'
 AND ID =@V.InlineSearchCustomer;
+
