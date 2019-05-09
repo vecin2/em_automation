eeee4e3eda68be883b6bff619a16cfa8f90746d3
vecin2 <vecin2@gmail.com>
2019-05-09 19:18:50 -0400

    fixing localised field issue

--- sql_gen/templates/add_entity_definition.sql
+++ sql_gen/templates/add_entity_definition.sql
@@ -1,13 +1,14 @@
 {% set tmp = entity_id | description("Please enter the entity_id, e.g Policy,PRJCustomer - do not add 'ED' at the end")%}
+{% set entity_name=entity_id+"ED" %}
 {% set default_display_name = entity_id | split_uppercase() %}
 {% set display_name = tmp1 | description("display_name") 
 			   | default(default_display_name)%}
 INSERT INTO EVA_ENTITY_DEFINITION (ID, ENV_ID, NAME, UUID, TYPE_UUID, TYPE_ID, TYPE_ENV_ID, LOGICAL_OBJ_PATH, INTERFACE_PATH, SUPER_ENTITY_DEFINITION, SUPER_ENTITY_DEFINITION_ENV_ID, IS_DELETED, IS_BASIC, SUPPORTS_READONLY,ICON_PATH, INSTANCE_ICON_PATH) VALUES (
 @ED.{{entity_id}}, -- ID
 @ENV.Dflt, -- ENV_ID
-'{{entity_id}}ED', -- NAME
-'{{entity_id}}', -- UUID
-'{{entity_id}}', -- TYPE_UUID
+'{{entity_name}}', -- NAME
+'{{entity_name}}', -- UUID
+'{{entity_name}}', -- TYPE_UUID
 @ET.{{entity_id}}, -- TYPE_ID
 @ENV.Dflt, -- TYPE_ENV_ID
 '{{logical_object_path  | codepath() | replace(".xml","")}}', -- LOGICAL_OBJECT_PATH
@@ -28,7 +29,7 @@ INSERT INTO EVA_CATEGORY_ENTRY(CATEGORY_ID, CATEGORY_ENV_ID, ENTITY_ID, ENTITY_E
 );
 
 {% set object_type ="EntityDefinitionED" %}
-{% set object_instance = entity_id %}
+{% set object_instance = entity_name %}
 {% set object_version = "@ED." +entity_id %}
 {% set field_name = "displayName" %}
 {% set text = display_name %}
