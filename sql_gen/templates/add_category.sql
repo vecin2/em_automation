2e79aeb68d3838f2a79397daf69e2a2461aa3840
vecin2 <vecin2@gmail.com>
2019-05-10 08:01:03 -0400

    add category

---
+++ sql_gen/templates/add_category.sql
@@ -0,0 +1,13 @@
+INSERT INTO EVA_CATEGORY (ID,ENV_ID,PARENT_CATEGORY_ID,PARENT_CATEGORY_ENV_ID,ENTITY_DEF_ID,ENTITY_DEF_ENV_ID,RELEASE_ID,SYSTEM_NAME)
+VALUES (
+	@EC.{{category_id}}, --ID
+	@ENV.Dflt, --ENV_ID
+	@EC.{{parent_category_id | suggest(_keynames.ED)}}, --PARENT_CATEGORY_ID
+	@ENV.Dflt, --PARENT_CATEGORY_ENV_ID
+	@ED.EntityDefinition, --ENTITY_DEF_ID
+	@ENV.Dflt, --ENTITY_DEF_ENV_ID
+	@RELEASE.ID,
+	{% set parent_category = _db.find.category_by_id(parent_category_id) %}
+	'{{parent_category["SYSTEM_NAME"]+"->"+category_id}}' --SYSTEM_NAME
+       );
+
