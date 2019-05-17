ec653f736784b9cb0cd5bb126a9c1f0e3fe487ff
David Álvarez García <vecin2@gmail.com>
2019-05-17 12:40:23 -0400

    testing commit

--- windist.bat
+++ windist.bat
@@ -1,3 +1,2 @@
 echo off
-
 pyinstaller sql_gen/__main__.py --onefile --hidden-import sql_gen.log.handlers --hidden-import sql_gen.sqltask_jinja.filters.default --hidden-import sql_gen.sqltask_jinja.filters.codepath --hidden-import sql_gen.sqltask_jinja.filters.description --hidden-import sql_gen.sqltask_jinja.filters.suggest --hidden-import sql_gen.sqltask_jinja.filters.other]  --add-data sql_gen/log/logging.yaml;sql_gen/log --name dtask
