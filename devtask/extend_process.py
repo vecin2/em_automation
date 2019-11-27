493678a772720a989b1d82a29405b88fe02735c5
vecin2 <vecin2@gmail.com>
2019-11-27 13:03:39 -0500

    making an empty process

--- devtask/extend_process.py
+++ devtask/extend_process.py
@@ -2,13 +2,23 @@ import pathlib
 from shutil import copyfile
 import os
 import sys
+import io
 
 import lxml.etree as ET
 
 #parser = ET.XMLParser(strip_cdata=False)
 #tree =ET.parse(filepath,parser)
 def is_process(process_path):
-    return "PackageEntry" in str(process_path.read_text())
+    parser = ET.XMLParser(encoding="utf-8")
+    text = process_path.read_text()
+    str_text = text.encode("utf-8")
+    import pdb;pdb.set_trace() 
+    tree = ET.fromstring(str_text).getroottree()
+    assert "dd" == type(tree)
+    doctype ==tree.docinfo.doctype
+    print("doctype is: "+doctype)
+    return "ProcessDefinition" == doctype
+    #return "PackageEntry" in str(process_path.read_text())
 
 def extend_process(process_path,target=None):
     if not process_path.exists():
--- devtask/test/test_extend_process.py
+++ devtask/test/test_extend_process.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pytest
 import pathlib
+import io
 
 import lxml.etree as ET
 from lxml import objectify
@@ -26,7 +27,7 @@ class AppRunner(object):
     def displays_message(self,message):
         assert self.capsys.readouterr().out == message
 
-    def file_exists(self,filepath,contents):
+    def assert_file_exists(self,filepath,contents):
         if not filepath.exists():
             assert False, "'"+str(filepath)+"' does not exists"
         assert contents == str(filepath.read_text())
@@ -48,13 +49,7 @@ def test_returnserror_when_file_not_a_process(app_runner,fs):
     app_runner.extend_process(process_path)
     app_runner.displays_message("Not a valid xml process found under '"+str(process_path)+"'\n")
 
-header ="""
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE ProcessDefinition [] >
-"""
-def empty_process():
-    root = ET.Element('PackageEntry')
-
+def new_process_def(parent):
     attribs ={
             "appearsInHistory":"true",
             "cyclic":"false",
@@ -69,46 +64,60 @@ def empty_process():
             "transactionBehaviour":"TX_NOT_SUPPORTED",
             "version":"10",
             "waitOnChildren":"false"}
-    pd =ET.SubElement(root,"ProcessDefinition",attribs)
-    attribs ={
-            "displayName":"",
-             "name":"",
-             "x":"16",
-             "y":"32",
-             }
-    #return  header + ET.tostring(root, pretty_print="true",encoding="unicode")
+    return ET.SubElement(parent,"ProcessDefinition",attribs)
+
+def empty_process():
+    root = ET.Element('PackageEntry')
+    process_def = new_process_def(root)
+    ET.SubElement(process_def,"StartNode",displayName="",name="",x="16",y="32")
+    ET.SubElement(process_def,"EndNode",displayName="",name="",x="240",y="32")
+    transition = ET.SubElement(process_def,"Transition",isExceptionTransition="false")
+    ET.SubElement(transition,"StartNodeReference",name="")
+    ET.SubElement(transition,"EndNodeReference",name="")
+    graph_node_list = ET.SubElement(transition,"GraphNodeList",name="")
+    graph_node = ET.SubElement(graph_node_list,
+                               "GraphNode",
+                               icon="",
+                               isLabelHolder="true",
+                               label="",
+                               name="",
+                               x="128",
+                               y="32")
+    ET.SubElement(process_def,"BuilderInfo",name="")
+    ET.SubElement(process_def,"TopicScope",defineTopicScope="false",name="")
     return ET.tostring(root,
                 pretty_print=True,
+                doctype='<!DOCTYPE ProcessDefinition [] >',
+                encoding ="UTF-8",
                 xml_declaration=True,
-                doctype='<!DOCTYPE ProcessDefinition [] >').decode("utf-8")
+                ).decode("utf-8")
 
-def test_empty_process():
-    empty_process_template = Path("devtask/test/templates/EmptyProcess.xml")
+def xml_str(filepath):
+    empty_process_template = Path(filepath)
     parser = ET.XMLParser(remove_blank_text=True)
     et =ET.parse(str(empty_process_template),parser)
-    root = et.getroot()
-    ET.ElementTree(root).write("output_original.xml",
-                             pretty_print=True,
-                             xml_declaration=True,
-                             encoding="UTF-8",
-                             doctype='<!DOCTYPE ProcessDefinition [] >',
-                            )
-    expected =  ET.tostring(root, pretty_print="true",encoding="unicode",doctype=et.docinfo.doctype)
-    empty_process_text = empty_process()
+    return tostring(et.getroot())
 
-    with open("output.xml","+w") as f:
-        f.write(empty_process_text)
+def tostring(element):
+    xml_declaration ="<?xml version='1.0' encoding='UTF-8'?>"
+    doctype='<!DOCTYPE ProcessDefinition [] >'
+    return xml_declaration+ "\n" +ET.tostring(element, pretty_print="true",encoding="unicode",doctype=doctype)
 
-    assert  expected == empty_process()
+def test_empty_process():
+    assert  empty_process() == xml_str("devtask/test/templates/EmptyProcess.xml")
+
+def test_parse_doctype():
+    filepath =Path(__file__).parent
+    process_path =filepath/Path("templates/EmptyProcess.xml")
+    et = ET.parse(str(process_path))
+    assert "bla" ==et.docinfo.doctype
 
 def test_extend_process_by_copy_copies_process_to_project_path(app_runner,fs):
     """"""
     process_path =Path("/repository/default/CoreEntities/EmptyProcess.xml")
     source_content=empty_process()
     fs.create_file(str(process_path), contents=source_content)
-    assert process_path.exists()
-
     target =Path("/repository/default/PRJEntities/Account")
     app_runner.extend_process(process_path,target)
-    app_runner.file_exists(target/"EmptyProcess.xml",
+    app_runner.assert_file_exists(target/"EmptyProcess.xml",
                            contents=source_content)
