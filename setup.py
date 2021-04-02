491d27199b062658975776085edfde96c3aec368
vecin2 <vecin2@gmail.com>
2021-04-02 16:50:52 -0400

    rename to em-sqltask

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sqltask",
+    name="em-sqltask",
     version="0.0.1a48",
     author="David Alvarez Garcia",
     author_email="david.avgarcia@gmail.com",
