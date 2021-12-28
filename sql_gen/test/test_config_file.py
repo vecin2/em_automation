bc57ba49b4602871638f7a65b94e37a231b1bbfb
vecin2 <vecin2@gmail.com>
2021-12-28 00:10:28 -0500

    fix duplicate config issue

--- sql_gen/test/test_config_file.py
+++ sql_gen/test/test_config_file.py
@@ -12,3 +12,9 @@ def test_value_in_config(fs):
     fs.create_file("/home/config.properties", contents="home='/em/prj'")
     config_file = ConfigFile("/home/config.properties")
     assert "home" in config_file
+
+def test_duplicate_property_takes_last_value_assigned(fs):
+    fs.create_file("/home/config.properties",
+                              contents="home=/em/prj\nhome=/opt/appserver")
+    config_file= ConfigFile("/home/config.properties")
+    assert "/opt/appserver"== config_file["home"]
