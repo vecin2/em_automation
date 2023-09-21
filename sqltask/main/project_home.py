c962f8ef18dc2b62394d566afc45047fb98755de
david.garcia <david.garcia@verint.com>
2023-09-21 09:28:55 +0200

    fix error handling exception when run outside em project

--- sqltask/main/project_home.py
+++ sqltask/main/project_home.py
@@ -16,14 +16,11 @@ class ProjectHome:
         return Path(self._emproject_home())
 
     def _emproject_home(self):
-        try:
-            return self._get_prj_home()
-        except Exception as excinfo:
-            sqltask.logger.error(str(excinfo))
-            exit(1)
+        return self._get_prj_home()
 
     def _get_prj_home(self):
         result = self._current_prj_path()
+        print("**************************")
 
         if not os.path.exists(result):
             error_msg = (
