cf97949b1fa7495a91faf3a70cc598127f14eabb
david.garcia <david.garcia@verint.com>
2023-10-16 07:41:55 +0200

    update from windows

--- windist.bat
+++ windist.bat
@@ -5,5 +5,7 @@ REM    Url is https://www.lfd.uci.edu/~gohlke/pythonlibs/#pymssql
 REM 2. The other issue we might encounter is that setuptools needs to be downgraded to 44.0.0
 echo off
 REM Please notice that all the dependecies must be installed. If using virtualenvwrapper use windows command line, not powershell
+git pull
 pyinstaller sqltask/main/__main__.py --onefile --hidden-import sqltask.log.handlers --hidden-import sqltask.sqltask_jinja.filters.default --hidden-import sqltask.sqltask_jinja.filters.codepath --hidden-import sqltask.sqltask_jinja.filters.description --hidden-import sqltask.sqltask_jinja.filters.suggest --hidden-import sqltask.sqltask_jinja.filters.other --hidden-import sqltask.sqltask_jinja.filters.print  --add-data sqltask/log/logging.yaml;sqltask/log --name sqltask
-copy dist\sqltask.exe %USERPROFILE%\AppData\Local\Microsoft\WindowsApps
+copy /Y dist\sqltask.exe \em\bin
+
